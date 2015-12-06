d7659c065cb742db52ce092127a75b8d89458752
mslatour <sanderlatour@gmail.com>
2015-12-06 16:36:42 +0100

    Tweaked syntax to comply with jslint and code style

--- app/scripts/controllers/apps.js
+++ app/scripts/controllers/apps.js
@@ -7,34 +7,33 @@
  * # AppsCtrl
  * Controller of the eduraamApp
  */
-var eduraam_apps_mock = [
+var eduraamAppsMock = [
 	{
 		'id': 0,
 		'name':'duolingo',
-		'description': "Leer nieuwe talen met behulp van deze leuke app.",
-		'logo': "https://upload.wikimedia.org/wikipedia/commons/thumb/1/15/Duolingo_logo.svg/2000px-Duolingo_logo.svg.png",
-		'url': "https://www.duolingo.com.app.eduraam.nl"
+		'description': 'Leer nieuwe talen met behulp van deze leuke app.',
+		'logo': 'https://upload.wikimedia.org/wikipedia/commons/thumb/1/15/Duolingo_logo.svg/2000px-Duolingo_logo.svg.png',
+		'url': 'https://www.duolingo.com.app.eduraam.nl'
 	},
 	{
 		'id': 1,
 		'name':'leestmeer',
-		'description': "Lekker lezen met behulp van deze leuke app.",
-		'logo': "http://www.leestmeer.nl/LeestmeerLogo.png",
-		'url': "http://platform.leestmeer.nl.app.eduraam.nl"
+		'description': 'Lekker lezen met behulp van deze leuke app.',
+		'logo': 'http://www.leestmeer.nl/LeestmeerLogo.png',
+		'url': 'http://platform.leestmeer.nl.app.eduraam.nl'
 	}
 ];
 
 angular.module('eduraamApp')
   .controller('AppsCtrl', ['$scope', '$location', '$routeParams',
 		function ($scope, $location, $routeParams) {
-			if( 'id' in $routeParams && $routeParams['id'] !== ""){
-				$scope.app_routed_url =
-					eduraam_apps_mock[parseInt($routeParams['id'])].url;
-
+			if( 'id' in $routeParams && $routeParams.id !== ''){
+				$scope.appRoutedUrl =
+					eduraamAppsMock[parseInt($routeParams.id)].url;
 			}else{
-				$scope.loadApp = function(app_id){
-					$location.path("/app/"+app_id+"/"); };
-				$scope.apps = eduraam_apps_mock;
+				$scope.loadApp = function(appId){
+					$location.path('/app/'+appId+'/'); };
+				$scope.apps = eduraamAppsMock;
 			}
 		}
   ]);
