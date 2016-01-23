3eba3e4019fbc8f0c4b262ad0832d9b485d9ce68
mslatour <sanderlatour@gmail.com>
2016-01-23 13:21:34 +0100

    Fixed missing trailing slash on api url.

--- app/scripts/services/apps.js
+++ app/scripts/services/apps.js
@@ -10,7 +10,7 @@
 angular.module('eduraamApp')
   .factory('Apps', ['$resource', 'envService',
   function ($resource, envService) {
-    var res = $resource(envService.read('apiUrl')+'/apps', null,
+    var res = $resource(envService.read('apiUrl')+'/apps/', null,
         {'all': { method:'GET', withCredentials: true }});
     res.getLaunchUrl = function(group, app){
       return envService.read('launchUrl')+'/'+group+'/'+app+'/';
