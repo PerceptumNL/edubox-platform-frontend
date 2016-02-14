ad8bd458ec7822cd93f04fa571c5314bf5e0db60
mslatour <sanderlatour@gmail.com>
2016-02-14 01:54:56 +0100

    Removed unused material icons.

--- test/karma.conf.js
+++ test/karma.conf.js
@@ -31,7 +31,6 @@ module.exports = function(config) {
       'bower_components/angular-route/angular-route.js',
       'bower_components/angular-sanitize/angular-sanitize.js',
       'bower_components/angular-material/angular-material.js',
-      'bower_components/angular-material-icons/angular-material-icons.min.js',
       'bower_components/angular-environment/dist/angular-environment.js',
       'bower_components/angular-http-auth/src/http-auth-interceptor.js',
       'bower_components/angular-mocks/angular-mocks.js',
