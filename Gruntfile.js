c487b5c7c15d75fade30e5a68fbb79638db07b69
mslatour <sanderlatour@gmail.com>
2016-02-14 02:32:55 +0100

    Disabled auto svg renaming for caching.

--- Gruntfile.js
+++ Gruntfile.js
@@ -261,7 +261,8 @@ module.exports = function (grunt) {
         src: [
           '<%= yeoman.dist %>/scripts/{,*/}*.js',
           '<%= yeoman.dist %>/styles/{,*/}*.css',
-          '<%= yeoman.dist %>/images/{,*/}*.{png,jpg,jpeg,gif,webp,svg}',
+          //'<%= yeoman.dist %>/images/{,*/}*.{png,jpg,jpeg,gif,webp,svg}',
+          '<%= yeoman.dist %>/images/{,*/}*.{png,jpg,jpeg,gif,webp}',
           '<%= yeoman.dist %>/styles/fonts/*'
         ]
       }
@@ -503,7 +504,7 @@ module.exports = function (grunt) {
   ]);
 
   grunt.registerTask('heroku', 'ALIAS. Running "build" task', function(target){
-	  grunt.task.run(['build:'+target]);
+    grunt.task.run(['build:'+target]);
   });
 
   grunt.registerTask('default', [
