d93fe5bf39b819540d6b12291bed8e96820e6ddd
mslatour <sanderlatour@gmail.com>
2016-02-26 17:46:41 +0100

    Fixed code conventions

--- app/scripts/controllers/unit.js
+++ app/scripts/controllers/unit.js
@@ -9,68 +9,68 @@
  */
 
 angular.module('eduraamApp')
-  .controller('UnitCtrl', ['$scope', '$routeParams', 'Units',
-      function ($scope, $routeParams, Units) {
+  .controller('UnitCtrl', ['$scope', '$routeParams', 'Units', 'URI',
+      function ($scope, $routeParams, Units, URI) {
         $scope.appLaunchUrl = Units.getLaunchUrl(
           $routeParams.group, $routeParams.unit);
         $scope.$on('$viewContentLoaded', function(){
-          angular.element(document.getElementById("app-frame"))
+          angular.element(document.getElementById('app-frame'))
             .on('load', function(){
-              console.log("Updating app frame links");
+              console.log('Updating app frame links');
               var routing = {
                 'code.org': true,
                 'scratch.mit.edu': true,
                 'google.com': true,
                 'google-analytics': true
               };
-              var routed_url = new URI(this.contentDocument.location.href);
-              var router_domain = routed_url.domain();
-              var token = routed_url.search(true).token;
-              var update_url_ = function(url, ignore_token){
-                if( url[0] == '/'){
-                  if(!ignore_token && token){
-                    return new URI(url).addQuery("token", token).toString();
+              var routedUrl = new URI(this.contentDocument.location.href);
+              var routerDomain = routedUrl.domain();
+              var token = routedUrl.search(true).token;
+              var updateUrl = function(url, ignoreToken){
+                if( url[0] === '/'){
+                  if(!ignoreToken && token){
+                    return new URI(url).addQuery('token', token).toString();
                   }
                 }else{
                   // convert url into a more manageable form.
-                  var url_obj = new URI(url);
-                  var host = url_obj.host()
+                  var urlObj = new URI(url);
+                  var host = urlObj.host();
                   // if host should be routed
-                  if( routing[host] || routing[url_obj.domain()] ){
+                  if( routing[host] || routing[urlObj.domain()] ){
                     // Construct the routed host to send the request to.
-                    var routed_host = "";
+                    var routedHost = '';
                     for (var i=0; i < host.length; i++) {
-                      routed_host += host.charCodeAt(i).toString(16);
+                      routedHost += host.charCodeAt(i).toString(16);
                     }
-                    routed_host += "."+router_domain;
-                    url_obj = url_obj.host(routed_host);
+                    routedHost += '.'+routerDomain;
+                    urlObj = urlObj.host(routedHost);
 
                     // Add the token if applicable.
-                    if(!ignore_token && token){
-                      url_obj = url_obj.addQuery("token", token)
+                    if(!ignoreToken && token){
+                      urlObj = urlObj.addQuery('token', token);
                     }
-                    return url_obj.toString();
+                    return urlObj.toString();
                   }
                 }
                 // If no adaption rule applied, return the original url
                 return url;
-              }
+              };
               // Update any jQuery ajax call, if applicable.
               if('jQuery' in this.contentWindow){
                 this.contentWindow.jQuery.ajaxPrefilter(function(options){
-                  options.url = update_url(options.url);
+                  options.url = updateUrl(options.url);
                 });
               }
               // Update links in <a> tags
-              var a_tags = this.contentDocument.getElementsByTagName("a");
-              for(var i=0; i < a_tags.length; i++){
-                a_tags[i].href = update_url(a_tags[i].href);
-              };
+              var aTags = this.contentDocument.getElementsByTagName('a');
+              for(var a=0; a < aTags.length; a++){
+                aTags[a].href = updateUrl(aTags[a].href);
+              }
               // Update actions in <form> tags
-              var form_tags = this.contentDocument.getElementsByTagName("form");
-              for(var i=0; i < form_tags.length; i++){
-                form_tags[i].action = update_url(a_tags[i].action);
-              };
+              var formTags = this.contentDocument.getElementsByTagName('form');
+              for(var f=0; f < formTags.length; f++){
+                formTags[f].action = updateUrl(formTags[f].action);
+              }
             });
         });
       }
